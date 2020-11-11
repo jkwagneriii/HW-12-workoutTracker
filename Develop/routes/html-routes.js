2ada8f4e96a3c2b6b5e14c0b7c995e0a0d02c1db
Jake Wagner <jkwagneriii@gmail.com>
2020-11-11 16:44:24 -0600

    okayyy. got the html routes to show up.

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,14 +1,21 @@
-// DEPENDENCIES
-
+// PATH DEPENDENCY
 var path = require("path");
 
-// module.exports = function(app) {
-//     // Called when "Continue Workout" or "New Workout" is clicked on index page
-//     app.get("/exercise", function(req, res) {
-//       res.sendFile(path.join(__dirname, "../public/exercise.html"));
-//     });
-//     // Called when for stats page
-//     app.get("/stats", function(req, res) {
-//       res.sendFile(path.join(__dirname, "../public/stats.html"));
-//     });
-//   };
+module.exports = function(app){ 
+
+  // Index Route  
+  app.get("/",function(req,res){    
+      res.sendFile(path.join(__dirname,"../public/index.html"));
+  });
+  
+  // Exercise Route
+  app.get("/exercise",function (req,res){   
+      res.sendFile(path.join(__dirname,"../public/exercise.html"));
+  });
+
+  // Stats Route
+  app.get("/stats",function(req,res){   
+      res.sendFile(path.join(__dirname,"../public/stats.html"));
+  });
+
+}
--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,25 @@
-
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-// const db = require("./models");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/html-routes")(app);
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
 });
