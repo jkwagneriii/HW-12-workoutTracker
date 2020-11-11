4beee6291e47e1834bded4d39863703cf71eccba
Jake Wagner <jkwagneriii@gmail.com>
2020-11-11 10:06:23 -0600

    working on html routes before class starts

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,3 +1,14 @@
 // DEPENDENCIES
 
 var path = require("path");
+
+module.exports = function(app) {
+    // Called when "Continue Workout" or "New Workout" is clicked on index page
+    app.get("/exercise", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+    // Called when for stats page
+    app.get("/stats", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+  };
